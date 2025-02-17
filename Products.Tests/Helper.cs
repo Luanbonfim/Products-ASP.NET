f76ab4c0e1464d73ad2cb4b7b32a07023c490688
Luan <luan_mantelo@hotmail.com>
2025-02-17 17:20:23 -0300

    IdentityService tests and refactor

---
+++ Products.Tests/Helper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Products.Infrastructure.Persistence;
+
+namespace Products.Tests
+{
+    public static class Helper
+    {
+        public static DbContextOptions<T> GetInMemoryOptions<T>(string databaseName) where T : DbContext
+        {
+            return new DbContextOptionsBuilder<T>()
+                .UseInMemoryDatabase(databaseName: databaseName)
+                .Options;
+        }
+    }
+}
