d13afc2f9500a010d3564eef263c4958d2cf4d98
Luan <luan_mantelo@hotmail.com>
2025-03-25 10:41:12 -0300

    migrationg EF in program class

--- Products.API/Program.cs
+++ Products.API/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using Products.API.Extensions;
 using Products.Infrastructure.Persistence;
@@ -25,6 +26,26 @@ var app = builder.Build();
 
 // Configure the application
 app.ConfigureWebApplication();
+
+// Run migrations
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var userContext = services.GetRequiredService<UserDbContext>();
+        var productsContext = services.GetRequiredService<ProductsDbContext>();
+        
+        userContext.Database.Migrate();
+        productsContext.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating the database.");
+        throw;
+    }
+}
 
 app.Run();
 
